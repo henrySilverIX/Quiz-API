2827e328088dd0ee6d45b5888b49ee3fea5299e3
henrySilverIX <henrysilver.gold@proton.me>
2025-05-13 16:21:39 -0300

    feat: Terminei de adicionar as funcionalidade no quizz

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Quiz-API
+
+Este é um projeto simples de um aplicativo de quiz de perguntas e respostas que utiliza a <a href="https://opentdb.com/">Open Trivia Database 
+API</a> para buscar perguntas em tempo real. O usuário responde True ou False para cada pergunta por meio de uma interface
+gráfica feita com Tkinter.
+
+# 🚀 Tecnologias Utilizadas
+🐍 Python 3
+
+🖼️ Tkinter – Biblioteca nativa do Python para interface gráfica
+
+🌐 Requests – Para fazer chamadas HTTP à API
+
+🔠 HTML Unescape – Para decodificar caracteres HTML nas perguntas
+
+
+# 🧠 Fonte das Perguntas
+As perguntas são obtidas em tempo real da API:
+https://opentdb.com/api.php
+Parâmetros utilizados:
+
+amount=10 (quantidade de perguntas)
+
+type=boolean (respostas do tipo verdadeiro ou falso)
+
+# 💡 Funcionalidades
+✅ Perguntas em tempo real da internet
+
+✅ Interface gráfica intuitiva
+
+✅ Feedback visual para respostas corretas/incorretas
+
+✅ Contador de pontuação em tempo real
+
+✅ Fim de jogo com resultado final
--- ui.py
+++ ui.py
@@ -1,15 +1,15 @@
 from tkinter import *
 import sys
 import os
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
 
 
-
-
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain_v: QuizBrain):
+        self.quiz_question = quiz_brain_v
         #Window configuration
         self.window = Tk()
         self.window.title("Quizz")
@@ -21,34 +21,64 @@ class QuizInterface:
 
 
         #Canvas
-        self.canvas = Canvas(width=300, height=250)
-        self.canvas.create_text(150, 175, text="Zebras are white with black stripes", font=("Arial", 20, "italic"))
-        self.canvas.grid(row=1, columnspan=2)
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150, 125, width=280, text="Some question text", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
 
 
         #Buttons
         true_icon = PhotoImage(file=self.resource_path("images/true.png"))
-        self.true_button = Button(image=true_icon, highlightthickness=0, bg=THEME_COLOR)
+        self.true_button = Button(image=true_icon, highlightthickness=0, bg=THEME_COLOR, command=self.mark_true)
         self.true_button.grid(row=2, column=1, pady=20)
 
         false_icon = PhotoImage(file=self.resource_path("images/false.png"))
-        self.true_button = Button(image=false_icon, highlightthickness=0, bg=THEME_COLOR)
-        self.true_button.grid(row=2, column=0, pady=20)
+        self.false_button = Button(image=false_icon, highlightthickness=0, bg=THEME_COLOR, command=self.mark_false)
+        self.false_button.grid(row=2, column=0, pady=20)
 
-
+        # Update the question text on canvas
+        self.get_next_question()
 
         #Main loop to keep the window working
         self.window.mainloop()
+
+    def get_next_question(self):
+        if self.quiz_question.still_has_questions():
+            self.canvas.config(background="white")
+            self.score.config(text=f"Score: {self.quiz_question.score}")
+            q_text = self.quiz_question.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.config(background="white")
+            self.canvas.itemconfig(self.question_text, text=f"End of the game\nFinal Score: {self.quiz_question.score}")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+
+
+    def mark_true(self):
+        self.feedback(self.quiz_question.check_answer("True"))
+
+    def mark_false(self):
+        self.feedback(self.quiz_question.check_answer("False"))
+
+    def feedback(self, is_right):
+        if is_right:
+            self.canvas.config(background="green")
+        else:
+            self.canvas.config(background="red")
+        self.window.after(1000, self.get_next_question)
 
 
 
     # Função to find the path correctly
     def resource_path(self, relative_path):
-        """ Get absolute path to resource, works for dev and for PyInstaller """
+        """ Get an absolute path to resource, works for dev and for PyInstaller """
         if getattr(sys, 'frozen', False):
             # Executável
             return os.path.join(sys._MEIPASS, relative_path)
         else:
             # Script normal
             return os.path.join(os.path.abspath("."), relative_path)
+
